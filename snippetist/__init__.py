94b1b9f743f8ccfe7e44855a9c871ef90e7b2bb9
myfreeweb <me@myfreeweb.ru>
2011-10-30 17:00:26 +0400

    pep8

--- snippetist/__init__.py
+++ snippetist/__init__.py
@@ -5,19 +5,23 @@ import uuid
 import copy
 import os
 
+
 def readtpl(name):
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), name + '.mustache')) as fl:
         return fl.read()
+
 
 def mksnippet(src):
     src = src.split('\n')
     whitespc = len([True for ch in src[1] if ch == ' '])
     return {'name': src[0],
-            'cont': '\n'.join([ln[whitespc-1:] for ln in src[1:]]) + '\n'}
+            'cont': '\n'.join([ln[whitespc - 1:] for ln in src[1:]]) + '\n'}
+
 
 def tabify(snip):
     snip['cont'] = '\t' + '\n\t'.join(snip['cont'].split('\n'))[:-1]
     return snip
+
 
 def mkfiles(data, template, extension=''):
     files = {}
@@ -26,6 +30,7 @@ def mkfiles(data, template, extension=''):
         data.update(snip)
         files[snip['name'] + extension] = pystache.render(tpl, data)
     return files
+
 
 def process(txt, name):
     txt = txt.split('\n---\n')
